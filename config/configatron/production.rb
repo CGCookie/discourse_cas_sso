336bba5a66925ea5b52d935d2ec1bcca2604d386
Jonathan Williamson <jonathan@cgcookie.com>
2016-10-10 15:32:55 -0600

    Update production env config

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -1,4 +1,9 @@
-# Override your default settings for the Production environment here.
-#
-# Example:
-#   configatron.file.storage = :s3
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+
+end
