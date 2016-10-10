82649383d8dadc9681ba160061f2946680a2b10b
Jonathan Williamson <jonathan@cgcookie.com>
2016-10-10 13:37:25 -0600

    Add ominauth url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas,
-           url: 'http://casino.test'#,
+           url: 'https://auth.cgcookie.com'#,
            #on_single_sign_out: Proc.new { |request| LoginController.action(:single_sign_out).call request.env }
 end
