9947210f30cd255abc1c23cb9ad647dafa94c999
Jonathan Williamson <jonathan@cgcookie.com>
2016-10-10 16:31:23 -0600

    Revert "Update production env config"
    This reverts commit 336bba5a66925ea5b52d935d2ec1bcca2604d386.

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -1,9 +1,4 @@
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
-  config.logger = Logger.new(STDOUT)
-
-end
+# Override your default settings for the Production environment here.
+#
+# Example:
+#   configatron.file.storage = :s3
